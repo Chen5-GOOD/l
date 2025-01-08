86266ab55d6c0cc9bedb0deee6ff949397fed669
Chen5-GOOD <x5_yang@163.com>
2025-01-08 21:42:44 +0800

    Add files via upload

---
+++ 新建文件夹 (2)/script.js
@@ -0,0 +1,4 @@
+document.getElementById('confessButton').addEventListener('click', function() {
+    var response = document.getElementById('response');
+    response.textContent = "我等你很久了，我也喜欢你！💖";
+});
